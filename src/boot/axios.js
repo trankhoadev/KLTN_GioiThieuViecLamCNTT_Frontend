40663fbb547548bc32fa0a6c761db5c80bc4b04e
trankhoadev <trankhoadev@gmail.com>
2023-02-18 13:13:57 +0700

    Config lại axios và thiết kế Authentication store xử lí chức năng đăng nhập và đăng xuất

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,24 +1,48 @@
-import { boot } from 'quasar/wrappers'
-import axios from 'axios'
-
+import { boot } from "quasar/wrappers";
+import axios from "axios";
+import { useStoreAuthentication } from "src/stores/storeAuthentication";
 // Be careful when using SSR for cross-request state pollution
 // due to creating a Singleton instance here;
 // If any client changes this (global) instance, it might be a
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'https://api.example.com' })
+const api = axios.create({ baseURL: "http://172.16.55.111:3000/" });
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
 
-  app.config.globalProperties.$axios = axios
+  app.config.globalProperties.$axios = axios;
   // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
   //       so you won't necessarily have to import axios in each vue file
+  api.defaults.headers["token_bombom"] = useStoreAuthentication().accessToken;
 
-  app.config.globalProperties.$api = api
+  api.interceptors.request.use((request) => {
+    return request;
+  });
+
+  api.interceptors.response.use(async (response) => {
+    // if (response.data.code === 401) {
+    //   return await api
+    //     .post("/auth/refreshToken")
+    //     .then((re) => {
+    //       if (re.data === 1) {
+    //         return router.push("/");
+    //       } else {
+    //         return router.push("/");
+    //       }
+    //     })
+    //     .catch((err) => {
+    //       return router.push("/asl");
+    //     });
+    // }
+
+    return response;
+  });
+
+  app.config.globalProperties.$api = api;
   // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
   //       so you can easily perform requests against your app's API
-})
+});
 
-export { api }
+export { api };
---
+++ src/stores/storeAuthentication.js
@@ -0,0 +1,79 @@
+import { defineStore } from "pinia";
+import { api } from "src/boot/axios";
+
+export const useStoreAuthentication = defineStore("storeAuthentication", {
+  state: () => {
+    return {
+      isPwd: true,
+      isAdmin: false,
+      userName: null,
+      password: "123456",
+      accessToken: null,
+      isLoadingLogin: false,
+    };
+  },
+  // lưu lại các state sau, có load lại cũng không bị mất
+  persist: {
+    storage: localStorage,
+    paths: ["userName", "accessToken", "isAdmin"],
+  },
+  getters: {},
+  actions: {
+    async login(user, pass) {
+      try {
+        this.isLoadingLogin = true;
+        await api
+          .post("auth/login", { userName: user, password: pass })
+          .then((response) => {
+            if (response.data.status == 200) {
+              let data = response.data.data;
+              api.defaults.headers["token_bombom"] = data.accessToken; //set token_bombom cho headers
+              this.accessToken = data.accessToken;
+              this.isAdmin = data.userData.accessData.accessDataStaff.isAdmin;
+              localStorage.setItem("isLoggedIn", true);
+              localStorage.setItem("token", data.accessToken);
+              this.router.push("/");
+              this.password = "";
+              this.isLoadingLogin = false;
+            } else {
+              this.isLoadingLogin = false;
+              Notify.create({
+                message: response.data.message,
+                timeout: 2000,
+                icon: "mood_bad",
+                color: "negative",
+                position: "top",
+              });
+            }
+          });
+      } catch (err) {
+        if (err) throw err;
+        this.isLoadingLogin = false;
+        Notify.create({
+          message: err,
+          timeout: 2000,
+          position: "top",
+          color: "negative",
+          icon: "mood_bad",
+        });
+      }
+    },
+
+    logOut() {
+      if (api.defaults.headers.token_bombom) {
+        localStorage.clear();
+        api.post("auth/logout").then((result) => {
+          if (result.data) {
+            api.defaults.headers.token_bombom = null;
+            // localStorage.clear();
+            sessionStorage.clear();
+            this.router.push("/");
+            window.location.reload();
+          }
+        });
+      } else {
+        this.router.push("/");
+      }
+    },
+  },
+});
